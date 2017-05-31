773e4d986857724aed638d6bfdb2e79b855be236
Luis Fernando <luis_fernandods@hotmail.com>
2017-05-31 14:01:44 -0300

    Aula 8 Atividade 1

--- app/routes/foto.js
+++ app/routes/foto.js
@@ -2,8 +2,14 @@ module.exports = function (app) {
 
     var api = app.api.foto;
 
-    app.get('/v1/fotos', api.lista);
 
-    app.get('/v1/fotos/:id', api.buscaPorId);
+    app.route('/v1/fotos')
+        .get(api.lista)
+        .post(api.adiciona);
 
+
+    app.route('/v1/fotos/:id')
+        .get(api.buscaPorId)
+        .delete(api.removePorId)
+        .put(api.atualiza);
 }
